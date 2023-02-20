ef0e977cbef8326c57cb598e47e1146a21e1e4a3
Akshaydave04 <122218630+Akshaydave04@users.noreply.github.com>
2023-02-20 22:28:40 +0530

    comiting project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Todo from './componets/Todo.js';
+import './componets/style.css';
+
+function App() {
+  return (
+   <>
+   <Todo />
+</>  );
+}
+
+export default App;
